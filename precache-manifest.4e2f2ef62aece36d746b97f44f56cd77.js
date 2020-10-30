4dc879b7ccba617435b0e3165c4540fc9b42f88f
detya <de.agitya@prosa.ai>
2020-10-31 01:28:49 +0700

    Updates

---
+++ precache-manifest.4e2f2ef62aece36d746b97f44f56cd77.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da96e18e024394940c18570ffaee801e",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "beacb482f9f793350d5a",
+    "url": "/portfolio/static/css/2.1afe660f.chunk.css"
+  },
+  {
+    "revision": "de503c2a7e6ab6ad6048",
+    "url": "/portfolio/static/css/main.aaa74d49.chunk.css"
+  },
+  {
+    "revision": "beacb482f9f793350d5a",
+    "url": "/portfolio/static/js/2.a935e42c.chunk.js"
+  },
+  {
+    "revision": "b2df6f42d3b318f0b6ad3605dbfe5698",
+    "url": "/portfolio/static/js/2.a935e42c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de503c2a7e6ab6ad6048",
+    "url": "/portfolio/static/js/main.ed71bf43.chunk.js"
+  },
+  {
+    "revision": "8d5151d6cdd6ccebf727",
+    "url": "/portfolio/static/js/runtime-main.7dabe899.js"
+  }
+]);
